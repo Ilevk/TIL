d35e8215348489b9a387b6c0c124aa349322bf48
kcs93023 <kcs93023@gmail.com>
2019-02-25 15:13:39 +0900

    강의 보고 해봤는데 여전히 머리속에 완벽히 남진 않았음.. 계속 연습해보자.

--- C/Algorithm/acmicpc/10164/main.c
+++ C/Algorithm/acmicpc/10164/main.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
+#include <memory.h>
 
-#define SIZE 15
+#define SIZE 16
 
 int matrix[SIZE][SIZE];
 
 int solve(int y, int x){
-    if( y==1 || x==1 ) matrix[y][x] = 1;
+    if( y==1 || x==1 ) return matrix[x][y] = 1;
+    if(matrix[x][y] != -1) return matrix[x][y];
 
+    matrix[x][y] = solve(x-1,y) + solve(x, y-1);
+    return matrix[x][y];
+}
+
+int result(int row, int column, int k){
+    if(k==0){
+        return solve(row,column);
+    }  else {
+//        int krow = (k / column) + 1;
+//        int kcolumn = k % (column);
+        int krow = (k + column - 1) / column;
+        int kcolumn = (k % column);
+        if (kcolumn == 0) kcolumn = column;
+        return solve(krow, kcolumn) * solve(row - krow + 1, column - kcolumn + 1);
+    }
 }
 
 
 int main() {
     int row, column, k;
     scanf("%d %d %d", &row, &column, &k);
-//    int krow = (k/column)+1;
-//    int kcolumn = k%(column+1)+1;
-//    int result = (factorial(krow+kcolumn-2)/(factorial(krow-1) * factorial(kcolumn-1)))*(factorial(row+column-krow-kcolumn)/(factorial(row-krow) * factorial(column-kcolumn)));
-    printf("%d", result);
+    memset(matrix, -1, sizeof(matrix));
+    int re = result(row, column, k);
+    printf("%d\n", re);
 }
